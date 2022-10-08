5a4af05105877c5d6c870530d0f4e53c37c7c97b
Raku Numata <ryaryu3ry@yahoo.co.jp>
2022-10-08 19:53:12 +0900

    迷路を少し表示できた

--- Inc/maze.hpp
+++ Inc/maze.hpp
@@ -4,7 +4,18 @@
 #include <cstdint>
 
 class Maze{
-    
+    public:
+    /**
+     * @brief 二進数で↓←→↑
+     * 
+     */
+    uint8_t wall[16][16];
+    /**
+     * @brief 各区画のコスト
+     * 
+     */
+    uint8_t cost[16][16];
+
 };
 
 #endif
