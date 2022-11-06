979e2017b40d8d73eca39597a1e0dd750660f4d6
Raku Numata <ryaryu3ry@yahoo.co.jp>
2022-11-06 18:10:30 +0900

    mazeからprint部分を分離した

--- Inc/maze.hpp
+++ Inc/maze.hpp
@@ -44,10 +44,7 @@ class Maze{
 
     void wall_update(uint8_t x, uint8_t y, uint8_t w);
 
-    void print_wall_with_uint8_t(uint8_t (&n)[16][16]);
-    void print_wall_with_arrow(uint8_t (&n)[16][16], uint8_t (&current)[2], std::vector<std::vector<uint8_t>> question);
-    void print_cost();
-    void print_route(uint8_t x, uint8_t y, std::vector<std::vector<uint8_t>> question);
+    
 
 };
 
---
+++ Inc/print.hpp
@@ -0,0 +1,20 @@
+#ifndef PRINT_HPP_
+#define PRINT_HPP_
+
+#include <cstdint>
+#include <vector>
+#include "maze.hpp"
+
+class Print{
+    public:
+    Maze *maze;
+
+    Print(Maze new_maze);
+
+    void print_wall_with_uint8_t(uint8_t (&n)[16][16]);
+    void print_wall_with_arrow(uint8_t (&n)[16][16], uint8_t (&current)[2], std::vector<std::vector<uint8_t>> question);
+    void print_cost();
+    void print_route(uint8_t x, uint8_t y, std::vector<std::vector<uint8_t>> question);
+};
+
+#endif
