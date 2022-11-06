df79760a21728dc0878cb2b79d15c82dc2dd91ae
Raku Numata <ryaryu3ry@yahoo.co.jp>
2022-11-06 18:27:36 +0900

    mazelibrary.hppを追加

---
+++ Inc/mazelibrary.hpp
@@ -0,0 +1,13 @@
+/**
+ * 他のプロジェクトにインクルードするためのファイル
+ * 組み込みARMを想定
+*/
+#ifndef MAZELIBRARY_HPP_
+#define MAZELIBRARY_HPP_
+
+#include "direction.hpp"
+#include "maze.hpp"
+#include "method.hpp"
+#include "mouse.hpp"
+
+#endif
